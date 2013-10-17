cc0905dfaf9125f64653144b4262c395aae43e18
frederickk <ken.frederick@gmx.de>
2013-10-18 00:09:39 +0200

    updated books/_template with callbacks

--- books/_template/scripts/main.js
+++ books/_template/scripts/main.js
@@ -31,11 +31,11 @@
  */
 var ios;
 var device = {
-	isiPad: null,
-	isiPhone: null,
-	isiPod: null,
-	isSafari: null,
-	isAndroid: null
+isiPad: null,
+isiPhone: null,
+isiPod: null,
+isSafari: null,
+isAndroid: null
 };
 var orientation;
 
@@ -61,19 +61,17 @@ var scrollSelector = '.page';
 /*
  *	Callbacks
  */
-function onReady(current) {};
+function onReady(pageIndex) {};
 
 // onepage-scroll callbacks
 // Article
-function onArticleLoad(current) {};
-function onBeforeArticlePage(from, to) {};
-function onAfterArticlePage(from, to) {};
-function onArticlePage(from, to) {};
+function onArticleLoad(pageIndex) {};
+function onBeforeArticlePage(pageIndex) {};
+function onAfterArticlePage(pageIndex) {};
 // Gallery
-function onGalleryLoad(current) {};
-function onBeforeGalleryPage(from, to) {};
-function onAfterGalleryPage(from, to) {};
-function onGalleryPage(from, to) {};
+function onGalleryLoad(pageIndex) {};
+function onBeforeGalleryPage(pageIndex) {};
+function onAfterGalleryPage(pageIndex) {};
 
 
 /*
@@ -95,46 +93,46 @@ var bRedirected = false;
  */
 // $(function() {
 $(document).load(function() {
-	/*
-	 *	Orientation
-	 */
-	angle = 0; // orienation angle
-	isRotated = false;
+                 /*
+                  *	Orientation
+                  */
+                 angle = 0; // orienation angle
+                 isRotated = false;
 
-	ios = navigator.userAgent.match(/(iPhone)|(iPod)|(iPad)/);
-	device.isiPad	= (navigator.userAgent.match(/iPad/i) != null);
-	device.isiPhone  = (navigator.userAgent.match(/iPhone/i) != null);
-	device.isiPod	= (navigator.userAgent.match(/iPod/i) != null);
-	device.isSafari  = (navigator.userAgent.match(/Safari/i) != null);
-	device.isAndroid = (navigator.userAgent.match(/Android/i) != null);
+                 ios = navigator.userAgent.match(/(iPhone)|(iPod)|(iPad)/);
+                 device.isiPad	= (navigator.userAgent.match(/iPad/i) != null);
+                 device.isiPhone  = (navigator.userAgent.match(/iPhone/i) != null);
+                 device.isiPod	= (navigator.userAgent.match(/iPod/i) != null);
+                 device.isSafari  = (navigator.userAgent.match(/Safari/i) != null);
+                 device.isAndroid = (navigator.userAgent.match(/Android/i) != null);
 
-	// initial orientation check
-	orientation	= 'landscape';
-	orientationChange();
+                 // initial orientation check
+                 orientation	= 'landscape';
+                 orientationChange();
 
-	/*
-	 *	Check Connection status
-	 */
-	console.log( 'online', navigator.onLine );
-	if(!(navigator.onLine) && !bRedirected ) {
-		window.location = './oops.html';
-		bRedirected = true;
-	}	 
+                 /*
+                  *	Check Connection status
+                  */
+                 console.log( 'online', navigator.onLine );
+                 if(!(navigator.onLine) && !bRedirected ) {
+                 window.location = './oops.html';
+                 bRedirected = true;
+                 }
 
 
-	// keep all links within webapp
-	// crucial for testing purposes
-	var a = document.getElementsByTagName('a');
-	for( var i=0; i<a.length; i++ ) {
-		if( !a[i].onclick && a[i].getAttribute('target') != '_blank' ) {
-			a[i].onclick = function() {
-				window.location = this.getAttribute('href');
-				return false; 
-			}
-		}
-	}
+                 // keep all links within webapp
+                 // crucial for testing purposes
+                 var a = document.getElementsByTagName('a');
+                 for( var i=0; i<a.length; i++ ) {
+                 if( !a[i].onclick && a[i].getAttribute('target') != '_blank' ) {
+                 a[i].onclick = function() {
+                 window.location = this.getAttribute('href');
+                 return false;
+                 }
+                 }
+                 }
 
-});
+                 });
 
 
 
@@ -148,88 +146,88 @@ $(document).load(function() {
  *	DOM is ready
  */
 $(document).ready(function() {
-	/*
-	 *	Links
-	 */
-	// $('a').click( function(event) {
-	// 	event.preventDefault();
-	// 	window.location = $(this).attr('href');
-	// });
+                  /*
+                   *	Links
+                   */
+                  // $('a').click( function(event) {
+                  // 	event.preventDefault();
+                  // 	window.location = $(this).attr('href');
+                  // });
 
-	// http://stackoverflow.com/questions/7901679/jquery-add-target-blank-for-outgoing-link
-	$('a[href^="htt"]').each(function() {
-		var link = $(this).attr('href');
+                  // http://stackoverflow.com/questions/7901679/jquery-add-target-blank-for-outgoing-link
+                  $('a[href^="htt"]').each(function() {
+                                           var link = $(this).attr('href');
 
-		// within normal browser
-		// open external links in a new window/tab
-		$(this).attr('target', '_blank');
-	});
-
-
-	/*
-	 *	Misc
-	 */
-	loadArticle();
-	orientationChange();
-	
-	// get active page
-	onReady( $(scrollContainer + ' ' + scrollSelector + '.active').data('index') );
-
-	// serve up retina images
-	$('img').retina('@2x');
-
-	// set carousel globally
-	$('.carousel').carousel({
-		interval: 5000,
-		pause: 'false'
-	});
+                                           // within normal browser
+                                           // open external links in a new window/tab
+                                           $(this).attr('target', '_blank');
+                                           });
 
 
-	/*
-	 *	Events
-	 */
-	// $('a').bind('click touchstart', function(e) {
-		// alert("I'm a " + e.type);
-		// e.preventDefault();
-	// });
+                  /*
+                   *	Misc
+                   */
+                  loadArticle();
+                  orientationChange();
 
-	$('#main-menu').bind('click touchstart', function() {
-		console.log( '#main-menu' );
-		toggleGrid();
-	});
+                  // get active page
+                  onReady( $(scrollContainer + ' ' + scrollSelector + '.active').data('index') );
 
-	// if the link is a gallery link, then it should
-	// not only open the gallery, but to the right page
-	// $('.gallery-link').click(function() {
-	$('.gallery-link').bind('click touchstart', function() {
-		toggleGallery( $(this) );
-	});
-	$('#gallery-close').bind('click touchstart', function() {
-		toggleGallery( $(this) );
-	});
+                  // serve up retina images
+                  $('img').retina('@2x');
+
+                  // set carousel globally
+                  $('.carousel').carousel({
+                                          interval: 5000,
+                                          pause: 'false'
+                                          });
 
 
-	// http://stackoverflow.com/questions/1207008/how-do-i-lock-the-orientation-to-portrait-mode-in-a-iphone-web-application
-	// $(window)	
-	// 	.bind('orientationchange', function(){
-	// 		if (window.orientation % 180 == 0){
-	// 			$(document.body).css('-webkit-transform-origin', '')
-	// 				.css('-webkit-transform', '');			
-	// 		}
-	// 		else {
-	// 			if ( window.orientation > 0) { //clockwise
-	// 				$(document.body).css('-webkit-transform-origin', '200px 190px')
-	// 					.css('-webkit-transform',  'rotate(-90deg)');  
-	// 			}
-	// 			else {
-	// 				$(document.body).css('-webkit-transform-origin', '280px 190px')
-	// 						.css('-webkit-transform',  'rotate(90deg)'); 
-	// 			}
-	// 		}
-	// 	 })
-	// 	.trigger('orientationchange'); 
+                  /*
+                   *	Events
+                   */
+                  // $('a').bind('click touchstart', function(e) {
+                  // alert("I'm a " + e.type);
+                  // e.preventDefault();
+                  // });
 
-});
+                  $('#main-menu').bind('click touchstart', function() {
+                                       console.log( '#main-menu' );
+                                       toggleGrid();
+                                       });
+
+                  // if the link is a gallery link, then it should
+                  // not only open the gallery, but to the right page
+                  // $('.gallery-link').click(function() {
+                  $('.gallery-link').bind('click touchstart', function() {
+                                          toggleGallery( $(this) );
+                                          });
+                  $('#gallery-close').bind('click touchstart', function() {
+                                           toggleGallery( $(this) );
+                                           });
+
+
+                  // http://stackoverflow.com/questions/1207008/how-do-i-lock-the-orientation-to-portrait-mode-in-a-iphone-web-application
+                  // $(window)
+                  // 	.bind('orientationchange', function(){
+                  // 		if (window.orientation % 180 == 0){
+                  // 			$(document.body).css('-webkit-transform-origin', '')
+                  // 				.css('-webkit-transform', '');
+                  // 		}
+                  // 		else {
+                  // 			if ( window.orientation > 0) { //clockwise
+                  // 				$(document.body).css('-webkit-transform-origin', '200px 190px')
+                  // 					.css('-webkit-transform',  'rotate(-90deg)');
+                  // 			}
+                  // 			else {
+                  // 				$(document.body).css('-webkit-transform-origin', '280px 190px')
+                  // 						.css('-webkit-transform',  'rotate(90deg)');
+                  // 			}
+                  // 		}
+                  // 	 })
+                  // 	.trigger('orientationchange');
+
+                  });
 
 
 
@@ -265,7 +263,6 @@ function orientationChange() {
 		angle = 0;
 	}
 
-    console.log( '~~~ orientationChange(): ' + window.orientation + ' ~~~' );
 
 	/*
 	 *	Pages
@@ -289,40 +286,37 @@ function paginate() {
 	// 	direction: 'horizontal',
 	// 	touchTarget: '#navigation',
 
-	// 	onBeforePageSwitch: function() {
-	// 		onBeforeGalleryPage();
+    // onLoad: function(pageIndex) {
+    // 	onGalleryLoad(pageIndex);
+    // },
+	// 	beforeMove: function(pageIndex) {
+	// 		onBeforeGalleryPage(pageIndex);
 	// 	},
-	// 	onAfterPageSwitch: function() {
-	// 		onAfterGalleryPage();
-	// 	},
-	// 	onPageJump: function() {
-	// 		onGalleryPage();
+	// 	afterMove: function(pageIndex) {
+	// 		onAfterGalleryPage(pageIndex);
 	// 	}
 	// });
 
 	articleScroll = $(scrollContainer).onepage_scroll({
-		sectionContainer: scrollContainer + ' ' + scrollSelector,
-		easing: 'cubic-bezier(.02, .01, .47, 1)',
-		// first page = last page = easeOutBounce
-		animationTime: 300,
-		pagination: false,
-		updateURL: true,
-		direction: 'horizontal',
-		touchTarget: '#navigation',
+                                                      sectionContainer: scrollContainer + ' ' + scrollSelector,
+                                                      easing: 'cubic-bezier(.02, .01, .47, 1)',
+                                                      // first page = last page = easeOutBounce
+                                                      animationTime: 300,
+                                                      pagination: false,
+                                                      updateURL: true,
+                                                      direction: 'horizontal',
+                                                      touchTarget: '#navigation',
 
-		onLoad: function(current) {
-			onArticleLoad( current );
-		},
-		onBeforePageSwitch: function(from, to) {
-			onBeforeArticlePage(from, to);
-		},
-		onAfterPageSwitch: function(from, to) {
-			onAfterArticlePage(from, to);
-		},
-		onPageJump: function(from, to) {
-			onArticlePage(from, to);
-		}
-	});
+                                                      onLoad: function(pageIndex) {
+                                                      onArticleLoad(pageIndex);
+                                                      },
+                                                      beforeMove: function(pageIndex) {
+                                                      onBeforeArticlePage(pageIndex);
+                                                      },
+                                                      afterMove: function(pageIndex) {
+                                                      onAfterArticlePage(pageIndex);
+                                                      }
+                                                      });
 
 };
 
@@ -347,9 +341,9 @@ function scrollTo(element) {
 
 // ------------------------------------------------------------------------
 function toggleGallery(element) {
-	var index = (element != undefined) 
-		? $(element).data('index')
-		: 0;
+	var index = (element != undefined)
+    ? $(element).data('index')
+    : 0;
 	console.log( 'toggleGallery()' + index );
 
 	// fade the gallery in
@@ -360,18 +354,18 @@ function toggleGallery(element) {
 // are triggered, yeah! limited jquery
 function toggleAuthor(val) {
 	$('.article-author-container').css(
-		'opacity', (val) ? 1.0 : 0.0
-	);
+                                       'opacity', (val) ? 1.0 : 0.0
+                                       );
 };
 function toggleSubject(val) {
 	$('#subject-container').css(
-		'opacity', (val) ? 1.0 : 0.0
-	);
+                                'opacity', (val) ? 1.0 : 0.0
+                                );
 };
 function toggleInterview(val) {
 	$('.article-interview-container').css(
-		'opacity', (val) ? 1.0 : 0.0
-	);
+                                          'opacity', (val) ? 1.0 : 0.0
+                                          );
 };
 
 function toggleGrid() {
@@ -385,10 +379,10 @@ function toggleGrid() {
  */
 function loadArticle(structure) {
 	structure = (structure == undefined)
-		? article
-		: structure;
-
-	console.log( 'loadArticle( ' + structure + ' )' ); 
+    ? article
+    : structure;
+    
+	console.log( 'loadArticle( ' + structure + ' )' );
 	//
 	// load article json
 	//
@@ -399,31 +393,31 @@ function loadArticle(structure) {
 	else {
 		console.log( 'loadArticle() getJSON( ' + filename + ' )' );
 		$.getJSON('copy/'+filename, function(data) {
-			// info block
-			subject = data.article.info.subject;
-			title = data.article.info.title;
-			author = data.article.info.author;
-			// introduction paragraphs
-			intro = data.article.intro.text;
-			// content
-			main = data.article.main.text;
-			interview = data.article.interview.text;
-			// images
-			images = data.article.captions.text;
-
-			structure = {
-				subject: 	jsonToHtml( subject, 'subject' ),
-				title:		jsonToHtml( title, 'title' ),
-				author: 	jsonToHtml( author, 'author' ),
-				intro: 		jsonToHtml( intro, 'intro' ),
-				main:		jsonToHtml( main, 'main' ),
-				interview:	jsonToHtml( interview, 'interview' ),
-				images:		jsonToHtml( images, 'captions' )
-			};
-			
-		});
+                  // info block
+                  subject = data.article.info.subject;
+                  title = data.article.info.title;
+                  author = data.article.info.author;
+                  // introduction paragraphs
+                  intro = data.article.intro.text;
+                  // content
+                  main = data.article.main.text;
+                  interview = data.article.interview.text;
+                  // images
+                  images = data.article.captions.text;
+                  
+                  structure = {
+                  subject: 	jsonToHtml( subject, 'subject' ),
+                  title:		jsonToHtml( title, 'title' ),
+                  author: 	jsonToHtml( author, 'author' ),
+                  intro: 		jsonToHtml( intro, 'intro' ),
+                  main:		jsonToHtml( main, 'main' ),
+                  interview:	jsonToHtml( interview, 'interview' ),
+                  images:		jsonToHtml( images, 'captions' )
+                  };
+                  
+                  });
 	}
-
+    
 	return structure;
 };
 
@@ -436,7 +430,7 @@ function jsonToHtml(arr, idName) {
 	var id = '';
 	for(i=0; i<arr.length; i++) {
 		id = '#'+idName+(i).toString();
-
+        
 		if( idName == 'captions' ) {
 			// handle captions a bit diffrently
 			// they are treated like lists
@@ -455,7 +449,7 @@ function jsonToHtml(arr, idName) {
  *	Cookies
  *	http://www.quirksmode.org/js/cookies.html
  */
- function saveCookie(name, value, days) {
+function saveCookie(name, value, days) {
 	if (days) {
 		var date = new Date();
 		date.setTime(date.getTime() + (days*24*60*60*1000));
@@ -465,7 +459,7 @@ function jsonToHtml(arr, idName) {
 	document.cookie = name + '=' + value + expires + '; path=/';
 };
 
- function openCookie(name) {
+function openCookie(name) {
 	var nameEQ = name + '=';
 	var ca = document.cookie.split(';');
 	for(var i=0;i < ca.length;i++) {
@@ -476,7 +470,7 @@ function jsonToHtml(arr, idName) {
 	return null;
 };
 
- function deleteCookie(name) {
+function deleteCookie(name) {
 	saveCookie(name, '', -1);
 };
 
@@ -506,16 +500,16 @@ function deleteSession(name) {
 
 // ------------------------------------------------------------------------
 //
-// Events	
+// Events
 //
 // ------------------------------------------------------------------------
 $(window).resize(function() {
-	orientationChange();
-});
+                 orientationChange();
+                 });
 
 
 // ------------------------------------------------------------------------
-$(window).scroll(function() { 
-}); 
+$(window).scroll(function() {
+                 });
 
 
